12ecfd7d15be7bdf65f999630c99466fbad633ab
Thomas Mongstad <thomasmongstad@gmail.com>
2022-03-23 10:51:08 +0100

    Update code.py
    Added relay to block studio monitors

--- code.py
+++ code.py
@@ -1,51 +1,81 @@
 import board
-from digitalio import DigitalInOut, Pull
+from digitalio import DigitalInOut, Pull, Direction
 from adafruit_debouncer import Debouncer
-pins = (
+
+monitor_relay = DigitalInOut(board.GP15)
+monitor_relay.direction = Direction.OUTPUT
+monitor_relay.value = True #Sets pin high, and NC will have power at relay.
+
+mic_pins = (
     board.GP0, 
     board.GP1, 
     board.GP2, 
     board.GP3, 
+)
+
+playout_pins = (
     board.GP4, 
     board.GP5, 
     board.GP6, 
     board.GP7,
 )
-faders = []   # will hold list of Debouncer objects
 
-for pin in pins:   # set up each pin
-    tmp_pin = DigitalInOut(pin) # defaults to input
-    tmp_pin.pull = Pull.UP      # turn on internal pull-up resistor
-    faders.append( Debouncer(tmp_pin) )
+mic_faders = []   # will hold list of Debouncer objects
+for mic_pin in mic_pins:   # set up each pin
+    mic_tmp_pin = DigitalInOut(mic_pin) # defaults to input
+    mic_tmp_pin.pull = Pull.UP      # turn on internal pull-up resistor
+    mic_faders.append( Debouncer(mic_tmp_pin) )
 
-opened = [
-    "Fader 1 open\r", 
-    "Fader 2 open\r", 
-    "Fader 3 open\r", 
-    "Fader 4 open\r", 
-    "Fader 5 open\r", 
+playout_faders = []   # will hold list of Debouncer objects
+for playout_pin in playout_pins:   # set up each pin
+    playout_tmp_pin = DigitalInOut(playout_pin) # defaults to input
+    playout_tmp_pin.pull = Pull.UP      # turn on internal pull-up resistor
+    playout_faders.append( Debouncer(playout_tmp_pin) )
+
+mic_opened = [
+    "HOST_LIVE ON\r", 
+    "GUEST_LIVE ON\r", 
+    "\r", 
+    "\r", 
+    ]
+
+
+mic_closed = [
+    "HOST_LIVE OFF\r", 
+    "GUEST_LIVE OFF\r", 
+    "\r", 
+    "\r", 
+    ]
+
+playout_opened = [
     "CARTWALL SHOW\r", 
+    "\r", 
     "PLAYER 1-1 START\r", 
     "PLAYER 1-2 START\r",
     ]
 
 
-closed = [
-    "Fader 1 closed\r", 
-    "Fader 2 closed\r", 
-    "Fader 3 closed\r", 
-    "Fader 4 closed\r", 
-    "Fader 5 closed\r", 
+playout_closed = [ 
     "CARTWALL HIDE\r", 
+    "\r", 
     "PLAYER 1-1 STOP\r", 
     "PLAYER 1-2 STOP\r",
     ]
 
-print("klar")
+
 while True:
-    for i in range(len(faders)):
-        faders[i].update()
-        if faders[i].fell:
-            print(opened[i])
-        if faders[i].rose:
-            print(closed[i])
+    for i in range(len(mic_faders)):
+        mic_faders[i].update()
+        if mic_faders[i].fell:
+            print(mic_opened[i])
+            monitor_relay.value = False
+        if mic_faders[i].rose:
+            print(mic_closed[i])
+            monitor_relay.value = True
+    
+    for i in range(len(playout_faders)):
+        playout_faders[i].update()
+        if playout_faders[i].fell:
+            print(playout_opened[i])
+        if playout_faders[i].rose:
+            print(playout_closed[i])
